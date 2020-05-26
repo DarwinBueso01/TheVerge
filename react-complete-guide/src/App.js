b0c3f4a73bfb89765bf4122ee08bda805a91037f
Darwin Bueso <darwingaldamez5@gmail.com>
2020-05-26 14:59:44 -0400

    removed styled components from app.js

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { id:'1', name: 'Max', age: 28 },
+      { id:'2', name: 'Manu', age: 29 },
+      { id:'3', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'Some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    ///const person = Object.assign({}, this.state.persons[personIndex]);  // Alternative to the spread operator
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  } 
+
+  deletePersonHandler = (personIndex) =>{
+    //const persons = this.state.persons.slice(); // Slice copies the full array and returns a new one stored in the persons constant variable.
+    const persons = [...this.state.persons]; // Alternative for the slice method, which uses the spread operator that spread the elements in the array into a list of elements which simply then gets added to the new array.
+    persons.splice(personIndex, 1); // Removes one element from the array
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    // const style ={
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
+            })} 
+          </div> 
+      );
+
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // Classes = ['red]
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // Classes = ['red', 'bold']
+    }
+
+    return (
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button className="button" onClick={this.togglePersonsHandler}>
+              Toggle Persons
+          </button>
+            {persons}
+        </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+  }
+}
+
+export default App;
